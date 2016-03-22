9deaf06b7fb87595963d17e7ccea40f0028bb61f
loredan1994 <loredan.calimanu@yahoo.com>
2016-03-23 01:01:24 +0200

    calculate testetc

---
+++ JuniorMind/BankRate/BankRateTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BankRate
+{
+    [TestClass]
+    public class BankRateTests
+    {
+        [TestMethod]
+        public void RateForFirstMonth()
+        {
+            decimal rate = CalculateBankRate(200, 2, 12, 1);
+            Assert.AreEqual(102, rate);
+        }
+        decimal CalculateBankRate(decimal total, int periodInMonths, decimal interestPerYear,int currentMonth)
+        {
+            return total / periodInMonths + total * interestPerYear / 12 /100 ;
+        }
+        
+    }
+}
