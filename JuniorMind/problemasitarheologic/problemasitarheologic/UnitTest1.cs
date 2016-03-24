f06b6de378bc4550bc29c42c08f39902af1a862b
loredan1994 <loredan.calimanu@yahoo.com>
2016-03-25 01:19:21 +0200

    sitarheologic

--- JuniorMind/problemasitarheologic/problemasitarheologic/UnitTest1.cs
+++ JuniorMind/problemasitarheologic/problemasitarheologic/UnitTest1.cs
@@ -7,8 +7,24 @@ namespace problemasitarheologic
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Metoda1()
         {
+            decimal primatestare = Arieminima(3, 1, 3, 1, 3, 1);
+            Assert.AreEqual(18.000002, primatestare);
         }
+        decimal Arieminima(int xa, int xb, int xc, int ya, int yb, int yc)
+
+        {
+            decimal ab = ((xb - xa) ^ 2 + (yb - ya) ^ 2) ^ 1 / 2;
+            decimal ac = ((xc - xa) ^ 2 + (yc - ya) ^ 2) ^ 1 / 2;
+            decimal bc = ((xc - xb) ^ 2 + (yc - yb) ^ 2) ^ 1 / 2;
+            decimal p = (ab + ac + bc) / 2;
+            decimal ArieHeronpartial = (p * ((p - ab) * (p - ac) * (p - bc)));
+            decimal ArieHeron = ((ArieHeronpartial))^1/2;
+            return ArieHeron;
+
+        }
+     
     }
 }
+
