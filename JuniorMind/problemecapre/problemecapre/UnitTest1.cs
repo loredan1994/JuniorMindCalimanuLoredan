9a5c62f1fa186e1de459ba36be1d5a558c3263d2
loredan1994 <loredan.calimanu@yahoo.com>
2016-03-24 23:19:38 +0200

    first commit

---
+++ JuniorMind/problemecapre/problemecapre/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Calculation
+{
+    [TestClass]
+    public class CapreHayConsumption {
+
+        [TestMethod]
+        public void Kilogramedefan()
+        {
+
+            float primatestare = Calculintermediar(20, 2, 4, 5, 8);
+            Assert.AreEqual(1 , primatestare ) ; 
+        }
+        float Calculintermediar(float x, float q, float y, float w, float z)
+        {
+            float numardezile = x / q;
+            float numardecapre = y / w;
+            float calculintermediar = numardezile * numardecapre;
+            return calculintermediar / z;
+        }
+
+    }
+}
+
+
