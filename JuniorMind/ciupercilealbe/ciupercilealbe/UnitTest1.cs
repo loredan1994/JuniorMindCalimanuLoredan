31415f5980964f415fcd17285c1b97dee1fd2958
loredan1994 <loredan.calimanu@yahoo.com>
2016-03-25 00:21:52 +0200

    problemaciuperci

---
+++ JuniorMind/ciupercilealbe/ciupercilealbe/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Ciupercilealbe
+{
+    [TestClass]
+    public class CiupercileRosii
+    {
+        [TestMethod]
+        public void CiuperciTest()
+        {
+            int primatestare = CiupercileRosi(4,8) ;
+            Assert.AreEqual(32, primatestare);
+        }
+        int CiupercileRosi(int multiplicitatea, int numarciupercialbe)
+        {
+            int numarciupercirosii = multiplicitatea * numarciupercialbe ;
+            return numarciupercirosii;
+        }
+
+    }
+}
