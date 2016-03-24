ceba5efc23c27ef01b2b443f23322bec0ffa9173
loredan1994 <loredan.calimanu@yahoo.com>
2016-03-25 00:28:34 +0200

    problematrenrezolvata

---
+++ JuniorMind/problematren/problematren/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ProblemaCuTrenuri
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Verificaredistanta()
+        {
+            float primatestare = Calatorie(25);
+            Assert.AreEqual(12,5,primatestare);
+        }
+        public float Calatorie(float distanta)
+        {
+
+
+            return distanta / 2;
+        }
+    }
+}
