04fcc654d8d7202085d801379a8852cb54e8feeb
loredan1994 <loredan.calimanu@yahoo.com>
2016-03-30 00:02:22 +0300

    editgitignore
    edit

--- JuniorMind/problemasitarheologic/problemasitarheologic/UnitTest1.cs
+++ JuniorMind/problemasitarheologic/problemasitarheologic/UnitTest1.cs
@@ -9,17 +9,17 @@ namespace problemasitarheologic
         [TestMethod]
         public void Metoda1()
         {
-            decimal primatestare = Arieminima(3, 1, 3, 1, 3, 1);
+            double primatestare = Arieminima(3, 1, 3, 1, 3, 1);
             Assert.AreEqual(18.000002, primatestare);
         }
-        decimal Arieminima(int xa, int xb, int xc, int ya, int yb, int yc)
+        double Arieminima(int xa, int xb, int xc, int ya, int yb, int yc)
 
         {
             int ab = ((xb - xa) ^ 2 + (yb - ya) ^ 2) ^ 1 / 2;
             int ac = ((xc - xa) ^ 2 + (yc - ya) ^ 2) ^ 1 / 2;
             int bc = ((xc - xb) ^ 2 + (yc - yb) ^ 2) ^ 1 / 2;
             int p = (ab + ac + bc) / 2;
-            decimal ArieHeronpartial = 1 / 2 ^ (p * ((p - ab) * (p - ac) * (p - bc)));
+            double ArieHeronpartial = Math.Sqrt((p * ((p - ab) * (p - ac) * (p - bc))));
             return ArieHeronpartial;
            
 
