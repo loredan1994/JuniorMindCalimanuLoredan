1d2a3f4545cc0d0f1385d70bb2c39fb4fee2f05e
loredan1994 <loredan.calimanu@yahoo.com>
2016-03-30 22:05:55 +0300

    AthletRepetitionsproblem

---
+++ JuniorMind/Athletrepetitions/Athletrepetitions/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Athletrepetitions
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            double FirstTest = AthletRounds(2);
+            Assert.AreEqual(4,FirstTest);
+        }
+        double AthletRounds(int numberofrepetions)
+        {
+            return Math.Pow(numberofrepetions, 2);
+        }
+    }
+}
