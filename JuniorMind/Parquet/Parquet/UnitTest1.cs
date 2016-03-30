2c25bf0a9133fac75d4feb427d30fa08225a3ff8
loredan1994 <loredan.calimanu@yahoo.com>
2016-03-30 22:01:53 +0300

    parquetcomputation

---
+++ JuniorMind/Parquet/Parquet/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Parquet
+{
+    [TestClass]
+    public class ParquetTest
+    {
+        [TestMethod]
+        public void Numberofpieces()
+        {
+            double FirstTest = ParquetComputation(120, 15);
+            Assert.AreEqual(9.2, FirstTest);
+        }
+       public double ParquetComputation(double NxMroom, double AxBplate)
+        {
+            return 1.15 * NxMroom / AxBplate ;
+
+        }
+    }
+}
