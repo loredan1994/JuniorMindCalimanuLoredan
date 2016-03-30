f502b158b639ee68c06877c6eae123a78b66db2f
loredan1994 <loredan.calimanu@yahoo.com>
2016-03-30 23:22:22 +0300

    endoffirstset

---
+++ JuniorMind/Plaquetteproblem/Plaquetteproblem/UnitTest1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Plaquetteproblem
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(4, findPlaquette(6, 6, 4));
+        }
+        [TestMethod]
+        public void PavementSecondTest()
+        {
+            Assert.AreEqual(16, findPlaquette(8, 8, 2));
+        }
+        public double findPlaquette(int mlength, int nlength, int acubic)
+        {
+
+            double longitudelength = (double)mlength / acubic;
+            double  latitudelength = (double)nlength / acubic;
+            int cubicPlaquetteneeded = (int)(Math.Ceiling(longitudelength) * (Math.Ceiling(latitudelength)));
+
+            return cubicPlaquetteneeded;
+        }
+
+    }
+
+
+}
