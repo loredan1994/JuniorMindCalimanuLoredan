efdde2ec5d0b72d39b2972929b837b9a9cc35160
loredan1994 <loredan.calimanu@yahoo.com>
2016-03-31 20:40:49 +0300

    FarmersTerrain

---
+++ JuniorMind/FarmersTerrain/FarmersTerrain/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FarmersTerrain
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void InitialTerrain()
+        {
+            Assert.AreEqual(770, ComputationOfTheSquareSurface(770));        }
+        int ComputationOfTheSquareSurface(int length)
+        {
+           double delta =Math.Sqrt(Math.Pow(230,2)+4*770000));
+            return length = (int)(-230 + delta) / 2;
+        }
+    }
+}
