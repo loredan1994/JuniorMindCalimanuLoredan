73b60f3496cfb244d5444845d76830f470ace8ad
loredan1994 <loredan.calimanu@yahoo.com>
2016-03-31 18:25:08 +0300

    calculate daytime for short distance

--- JuniorMind/TaxiFare/TaxiFare/UnitTest1.cs
+++ JuniorMind/TaxiFare/TaxiFare/UnitTest1.cs
@@ -9,11 +9,11 @@ namespace TaxiFare
         [TestMethod]
         public void DayTimeFareForShortDistances()
         {
-            Assert.AreEqual(5,CalculateTaxiFare(8, 1));
+            Assert.AreEqual(5,CalculateTaxiFare(1, 8));
         }
-        decimal CalculateTaxiFare(int hour, int distanceInKm)
+        decimal CalculateTaxiFare(int distanceInKm, int hour)
         {
-            return 0;
+            return 5*distanceInKm;
         }
     }
 }
