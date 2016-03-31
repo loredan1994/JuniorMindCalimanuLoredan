a6b6632afa4a5c0b971bce776c1331e74d2e34af
loredan1994 <loredan.calimanu@yahoo.com>
2016-03-31 20:17:54 +0300

    fizzbuzzcalculus

--- JuniorMind/FizzBuzzproblem/FizzBuzzproblem/UnitTest1.cs
+++ JuniorMind/FizzBuzzproblem/FizzBuzzproblem/UnitTest1.cs
@@ -7,8 +7,37 @@ namespace FizzBuzzproblem
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Diviziblewith3()
         {
+            Assert.AreEqual("Fizz", ComputeAndDivide(9));
+        }
+        [TestMethod]
+        public void Divisiblewith5()
+        {
+            Assert.AreEqual("Buzz", ComputeAndDivide(20));
+        }
+        [TestMethod]
+        public void Diviziblewith3and5()
+        {
+            Assert.AreEqual("FizzBuzz", ComputeAndDivide(15));
+        }
+        public string ComputeAndDivide(int number)
+        {
+            string result = string.Empty;
+            result = ResultOfTheCalculus(number, result);
+            return result;
+        }
+
+        private static string ResultOfTheCalculus(int number, string result)
+        {
+            if (number % 3 == 0) result = "Fizz";
+
+            if (number % 5 == 0) result = "Buzz";
+
+            if ((number % 3 == 0) && (number % 5 == 0)) result = "FizzBuzz";
+            return result;
         }
     }
-}
+
+    }
+
