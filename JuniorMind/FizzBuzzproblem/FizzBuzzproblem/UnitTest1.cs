df144533f09485595c143343b94d7ba57a0f23df
loredan1994 <loredan.calimanu@yahoo.com>
2016-03-31 20:20:08 +0300

    editabitorders

--- JuniorMind/FizzBuzzproblem/FizzBuzzproblem/UnitTest1.cs
+++ JuniorMind/FizzBuzzproblem/FizzBuzzproblem/UnitTest1.cs
@@ -30,11 +30,14 @@ namespace FizzBuzzproblem
 
         private static string ResultOfTheCalculus(int number, string result)
         {
-            if (number % 3 == 0) result = "Fizz";
+            if (number % 3 == 0)
+               result = "Fizz";
 
-            if (number % 5 == 0) result = "Buzz";
+            if (number % 5 == 0)
+               result = "Buzz";
 
-            if ((number % 3 == 0) && (number % 5 == 0)) result = "FizzBuzz";
+            if ((number % 3 == 0) && (number % 5 == 0))
+              result = "FizzBuzz";
             return result;
         }
     }
