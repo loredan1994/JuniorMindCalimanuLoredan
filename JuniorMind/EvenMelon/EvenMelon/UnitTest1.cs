ae78cae6ee61f00a6b29c11998a9309fc7539465
loredan1994 <loredan.calimanu@yahoo.com>
2016-04-14 22:20:38 +0300

    melonproblem

---
+++ JuniorMind/EvenMelon/EvenMelon/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EvenMelon
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual("NO it's odd", CalculateIfTheWatermelonisEven(21));
+        }
+        [TestMethod]
+        public void MyTestMethod2()
+        {
+            Assert.AreEqual("YES it is even", CalculateIfTheWatermelonisEven(20));
+        }
+        public string CalculateIfTheWatermelonisEven(int melon)
+        {
+           
+
+            if ((melon > 2) && (melon % 2 == 0))
+            return "YES it is even";
+
+            else return "NO it's odd";
+        }
+    }
+}
