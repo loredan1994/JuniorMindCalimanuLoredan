52d8051e3db1942134c0e7241a2374f7d63a99db
loredan1994 <loredan.calimanu@yahoo.com>
2016-04-14 21:51:42 +0300

    commitroman

---
+++ JuniorMind/RomanNumbers/RomanNumbers/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RomanNumbers
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string firsttest = IntToRoman(10);
+            Assert.AreEqual(X, firsttest);
+        }
+        public string IntToRoman(int num)
+        {
+            string[] thou = { "", "M", "MM", "MMM" };
+            string[] hun = { "", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM" };
+            string[] ten = { "", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC" };
+            string[] ones = { "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX" };
+            string roman = "";
+            roman= thou[(int)(num / 1000) % 10] + hun[(int)(num / 100) % 10] + ten[(int)(num / 10) % 10] + ones[num % 10] ;
+            return roman;
+            
+        }
+    }
+
+}
