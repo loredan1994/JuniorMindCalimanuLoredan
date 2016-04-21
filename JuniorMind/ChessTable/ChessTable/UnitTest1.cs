5c4b12c08302f9d45d0c3a1a605e53bdda62b358
loredan1994 <loredan.calimanu@yahoo.com>
2016-04-21 21:50:22 +0300

    testsWorking

--- JuniorMind/ChessTable/ChessTable/UnitTest1.cs
+++ JuniorMind/ChessTable/ChessTable/UnitTest1.cs
@@ -7,13 +7,45 @@ namespace ChessTable
     public class UnitTest1
     {
         [TestMethod]
-        public void TestingForTwoLines()
+        public void TestingForTwoSquares()
         {
-            Assert.AreEqual(49, (GetNumberOfSquares(2)));
+            Assert.AreEqual(5, (GetNumberOfSquares(2)));
         }
-        public int GetNumberOfSquares(int squares)
+        [TestMethod]
+        public void TestingForThreeSquares()
         {
-            return 0;
+            Assert.AreEqual(14, (GetNumberOfSquares(3)));
+        }
+        [TestMethod]
+        public void TestingForFourSquares()
+        {
+            Assert.AreEqual(30, (GetNumberOfSquares(4)));
+        }
+        [TestMethod]
+        public void TestingForFiveSquares()
+        {
+            Assert.AreEqual(55, (GetNumberOfSquares(5)));
+        }
+        [TestMethod]
+        public void TestingForSixSquares()
+        {
+            Assert.AreEqual(91, (GetNumberOfSquares(6)));
+        }
+        [TestMethod]
+        public void TestingForSevenSquares()
+        {
+            Assert.AreEqual(140, (GetNumberOfSquares(7)));
+        }
+        public double GetNumberOfSquares(double squares)
+        {
+            double computation = 0;
+            while (squares > 0)
+            {
+                computation = computation + (squares* squares);
+                squares--;
+            }
+            return computation;
+
         }
     }
 }
