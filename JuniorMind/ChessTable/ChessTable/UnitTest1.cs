f2a527d919cf61e712db1acb2fca0d10b9274b2e
loredan1994 <loredan.calimanu@yahoo.com>
2016-04-21 21:21:44 +0300

    firsttestfail

---
+++ JuniorMind/ChessTable/ChessTable/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ChessTable
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestingForTwoLines()
+        {
+            Assert.AreEqual(49, (GetNumberOfSquares(2)));
+        }
+        public int GetNumberOfSquares(int squares)
+        {
+            return 0;
+        }
+    }
+}
