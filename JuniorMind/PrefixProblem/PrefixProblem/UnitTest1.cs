45d447e99a7c57e48207067ed43b2249399b130f
loredan1994 <loredan.calimanu@yahoo.com>
2016-04-21 22:11:56 +0300

    PrefixFirstTestFail

---
+++ JuniorMind/PrefixProblem/PrefixProblem/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PrefixProblem
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void FindPrefix()
+        {
+            Assert.AreEqual("abab", FindIfItsPrefix("ababc", "ababd"));
+        }
+        public string FindIfItsPrefix(string firstWord , string secondWord)
+        {
+            return string.Empty;
+        }
+    }
+}
