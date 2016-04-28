2bf1dd833ffa97560ff2ea871b4396c6f60ce12b
loredan1994 <loredan.calimanu@yahoo.com>
2016-04-28 19:04:28 +0300

    commitlotto

--- JuniorMind/Anagram/Anagram/UnitTest1.cs
+++ JuniorMind/Anagram/Anagram/UnitTest1.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace AnagramProblem
+namespace Anagram
 {
     [TestClass]
     public class UnitTest1
---
+++ JuniorMind/LottoProblem/LottoProblem/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LottoProblem
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(0.0000000715,GetProbabilities(6, 6, 49));
+        }
+        public double GetProbabilities(int favorableCases, int PossibleCases, int totalNumbers)
+        {
+            double probabilities = favorableCases / PossibleCases;
+            return probabilities;
+        }
+    }
+    
+}
