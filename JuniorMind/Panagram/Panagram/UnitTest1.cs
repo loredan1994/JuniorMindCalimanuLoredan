9773b17e6e16803e65be0d3e430aa0d62210e828
loredan1994 <loredan.calimanu@yahoo.com>
2016-04-28 21:18:29 +0300

    workingproperlyiThink

--- JuniorMind/Panagram/Panagram/UnitTest1.cs
+++ JuniorMind/Panagram/Panagram/UnitTest1.cs
@@ -7,8 +7,52 @@ namespace Panagram
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void CheckIfPanagram()
         {
+            Assert.AreEqual(true, CheckIfPanagram("The quick brown fox jumps over the lazy dog"));
         }
+        [TestMethod]
+        public void CalculatePanagram()
+        {
+            Assert.AreEqual(false, CheckIfPanagram("I Do not care"));
+        }
+        [TestMethod]
+        public void PanagramTesting()
+        {
+            Assert.AreEqual(true, CheckIfPanagram("ABCDEFGHIJKLMNOPQRSTUVWXYZ"));
+        }
+        [TestMethod]
+        public void FailedPanagramTesting()
+        {
+            Assert.AreEqual(false, CheckIfPanagram("cBCDEFGHIJKLMNOPQRSTUVWXYZ"));
+        }
+        public bool CheckIfPanagram(string statement)
+        {
+            string makeLowCaseAll = statement.ToLower();
+            if (TestIfPanagram(makeLowCaseAll))
+                return true;
+            return false;
+        }
+        public bool TestIfPanagram(string panagramStatement)
+        {
+
+            for (int i = 'a'; i < 'z'; i++)
+
+            {
+                int counter = 0;
+
+                for (int j = 0; j < panagramStatement.Length; j++)
+                {
+                    if (panagramStatement[j] == i) counter++;
+                }
+                if (counter == 0)
+                    return false;
+
+            }
+
+            return true;
+
+        }
+
     }
 }
