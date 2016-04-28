3e596a674f82a8999be4eb60bf22b231e590b671
loredan1994 <loredan.calimanu@yahoo.com>
2016-04-28 19:14:42 +0300

    commit

--- JuniorMind/LottoProblem/LottoProblem/UnitTest1.cs
+++ JuniorMind/LottoProblem/LottoProblem/UnitTest1.cs
@@ -11,10 +11,32 @@ namespace LottoProblem
         {
             Assert.AreEqual(0.0000000715,GetProbabilities(6, 6, 49));
         }
+        [TestMethod]
+        public void ComputeFactorial()
+        {
+            Assert.AreEqual(24, GetFactorial(4));
+        }
         public double GetProbabilities(int favorableCases, int PossibleCases, int totalNumbers)
         {
             double probabilities = favorableCases / PossibleCases;
             return probabilities;
+        }
+         public double combinationsOfNumbers(int favorableCases , int PossibleCases)
+        {
+            double combinations = GetFactorial(favorableCases) / GetFactorial(favorableCases - PossibleCases) * GetFactorial(PossibleCases);
+            return combinations;
+        }
+        public int GetFactorial(int number)
+        {
+            int result = 1;
+            for (int i = 1; i <= number; i++)
+            {
+                result *= i;
+
+            }
+
+            return result;
+
         }
     }
     
