c7a2a5b08f2a0dc47ffaaeea7c60dec903215004
loredan1994 <loredan.calimanu@yahoo.com>
2016-05-03 19:39:51 +0300

    FirstAttempt

--- JuniorMind/Lunch/Lunch/UnitTest1.cs
+++ JuniorMind/Lunch/Lunch/UnitTest1.cs
@@ -7,8 +7,34 @@ namespace Lunch
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void FidingTheDays()
         {
+            Assert.AreEqual(12, FindCMMMC(6, 4));
+        }
+        [TestMethod]
+        public void ForEqualNumberOfDays()
+        {
+            Assert.AreEqual(4, FindCMMMC(4, 4));
+        }
+        [TestMethod]
+        public void WhenFriendTwoIsBiggerThanFriendOne()
+        {
+            Assert.AreEqual(12, FindCMMMC(4, 6));
+        }
+        public int FindDifferenceOfDays(int FriendOne, int FriendTwo)
+        {
+            int smallDivider=0;
+            while (FriendOne != FriendTwo)
+            {
+                if(FriendOne>FriendTwo)
+                return FriendOne = FriendOne - FriendTwo;
+                return FriendTwo = FriendTwo - FriendOne;               
+            }
+            return smallDivider = FriendOne;
+        }
+        public int FindCMMMC( int FriendOne, int FriendTwo)
+        {
+            return FriendOne * FriendTwo / FindDifferenceOfDays(FriendOne, FriendTwo);
         }
     }
 }
