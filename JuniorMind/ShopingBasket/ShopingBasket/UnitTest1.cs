f67bc05c4cc54d5bd8d452d2029669c3cc6e45d0
loredan1994 <loredan.calimanu@yahoo.com>
2016-07-07 18:38:23 +0300

    ShopingBasketStart
    made the struct , going on with the other steps.

---
+++ JuniorMind/ShopingBasket/ShopingBasket/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ShopingBasket
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+         
+        }
+        public struct ShopingBasket
+        {
+            public string nameOfProduct;
+            public decimal priceOfProduct;
+            public ShopingBasket(string nameOfProduct, decimal priceOfProduct)
+            {
+                this.nameOfProduct = nameOfProduct;
+                this.priceOfProduct = priceOfProduct;
+            }
+
+        }
+
+    }
+}
