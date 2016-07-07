ad1e5bc1144b0ad51ee22fa420435bc53a61b229
loredan1994 <loredan.calimanu@yahoo.com>
2016-07-08 02:01:06 +0300

    intersection
    the start of intersection problem , though i got stuck kinda , i don't
    know what next.

---
+++ JuniorMind/Intersection/Intersection/UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Intersection
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            return ;
+        }
+        public enum Direction {
+            up,
+            down,
+            right,
+            left,
+        }
+        public struct Cursor
+        {
+            public int xAxis;
+            public int yAxis;
+            public Cursor( int xAxis , int yAxis)
+            {
+                this.xAxis = xAxis;
+                this.yAxis = yAxis;
+            }
+        }
+    }
+}
