3484e8cb40e72bb48bf7b901abf536826c814ffe
loredan1994 <loredan.calimanu@yahoo.com>
2016-07-14 19:59:45 +0300

    intersection
    this is what i meant , i used too many if conditions i think.

--- JuniorMind/Intersection/Intersection/UnitTest1.cs
+++ JuniorMind/Intersection/Intersection/UnitTest1.cs
@@ -9,7 +9,14 @@ namespace Intersection
         [TestMethod]
         public void TestMethod1()
         {
-            return ;
+            Direction[] firstIntersection = new Direction[]
+         {
+                Direction.up,
+                Direction.right,
+                Direction.down,
+                Direction.left
+         } ;
+            Assert.AreEqual(new Cursor(0, 0), ReturnPickCase(new Cursor(0,1), firstIntersection));
         }
         public enum Direction {
             up,
@@ -27,5 +34,36 @@ namespace Intersection
                 this.yAxis = yAxis;
             }
         }
+    public Cursor  ReturnPickCase ( Cursor cursor , Direction[] direction )
+        {
+            int xAxis = 0;
+            int yAxis = 0;
+            Cursor[] checkPoint = new Cursor[direction.Length];
+            for (int i = 0 ; i < direction.Length; i++)
+            {
+                if (direction[i] == Direction.up)    xAxis++;
+                if (direction[i] == Direction.down)  xAxis--;
+                if (direction[i] == Direction.left)  yAxis--;
+                if (direction[i] == Direction.right) yAxis++;
+                for(int j = 0; j< checkPoint.Length; j++)
+                {
+                    if (IntersectPointCheck(xAxis, yAxis, checkPoint, j)) return checkPoint[j];
+                }
+
+                checkPoint[i].xAxis = xAxis;
+                checkPoint[i].yAxis = yAxis;
+            }
+            return checkPoint[0];
+            
+        }
+        public static bool IntersectPointCheck( int xAxis , int yAxis , Cursor[] checkPoint, int  j )
+        {
+            { 
+            return (checkPoint[j].xAxis == xAxis && checkPoint[j].yAxis == yAxis) ;
+            }
+           
+
+        }
+
     }
 }
