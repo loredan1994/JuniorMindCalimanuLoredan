b583c8bf38049b8075176cfb20f46838af1b1eab
loredan1994 <loredan.calimanu@yahoo.com>
2016-07-15 00:21:47 +0300

    password start

--- JuniorMind/Password/Password/UnitTest1.cs
+++ JuniorMind/Password/Password/UnitTest1.cs
@@ -10,5 +10,29 @@ namespace Password
         public void TestMethod1()
         {
         }
+        public struct Password
+        {
+            public int passwordLength;
+            public int noUpperCaseLetters;
+            public int noLowerCaseLetters;
+            public int noOfNumbers;
+            public int noOfSymbols;
+            public bool withoutSimilarities;
+            public bool withoutAmbiguities;
+
+
+            public Password(int passwordLength, int noUpperCaseLetters, int noLowerCaseLetters, int noOfNumbers, int noOfSymbols, bool withoutSimilarities, bool withoutAmbiguities)
+            {
+                this.passwordLength = passwordLength;
+                this.noUpperCaseLetters = noUpperCaseLetters;
+                this.noLowerCaseLetters = noLowerCaseLetters;
+                this.noOfNumbers = noOfNumbers;
+                this.noOfSymbols = noOfSymbols;
+                this.withoutSimilarities = withoutSimilarities;
+                this.withoutAmbiguities = withoutAmbiguities;
+
+            }
+        }
     }
 }
+
