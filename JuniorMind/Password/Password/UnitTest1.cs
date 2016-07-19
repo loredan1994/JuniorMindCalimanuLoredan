a18688bf1169d84c3d16a97f66c2efc70e8108ac
loredan1994 <loredan.calimanu@yahoo.com>
2016-07-19 20:57:35 +0300

    test done

--- JuniorMind/Password/Password/UnitTest1.cs
+++ JuniorMind/Password/Password/UnitTest1.cs
@@ -7,8 +7,9 @@ namespace Password
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestPassword()
         {
+            Password password = new Password(40, 12, 5, 3, 20, true, false);
         }
         public struct Password
         {
@@ -36,7 +37,10 @@ namespace Password
         static Random password = new Random();
         public string PasswordGenerator(Password password)
         {
-            return string.Empty;
+            var excludeSimilarities = password.withoutSimilarities ? "l1Io0O" : string.Empty;
+            var excludeAmbiguities = password.withoutAmbiguities ? " {}[]()/\'~,;.<> " : string.Empty;
+            var Password = StringSimilarities(password.noLowerCaseLetters, 'a', 'z', excludeSimilarities) + StringSimilarities(password.noUpperCaseLetters, 'A', 'Z', excludeSimilarities) + StringSimilarities(password.noOfNumbers, '0', '9', excludeSimilarities) + GennerateSymbolPassword(password.noOfSymbols, excludeAmbiguities);
+            return Password;
         }
         public char GenerateCharacter( char minValue, char maxValue)
         {
@@ -48,6 +52,57 @@ namespace Password
             int random = password.Next(0, symbol.Length - 1);
             return symbol[random];
         }
-    }
+        public string GennerateSymbolPassword(int noOfSymbols , string without = "()/\'~,;.<>{}[] ")
+        {
+            string symbols = string.Empty;
+            int count = 0;
+            while (noOfSymbols > count)
+            {
+                char random = GenerateSymbol();
+                var excludeAmbiguities = without.Contains(random.ToString());
+                if (!excludeAmbiguities)
+                {
+                    symbols += random;
+                    count++;
+                }
+            }
+            return symbols;
+        }
+        public string StringSimilarities(int noOfChar , char first , char last , string without = "l1Io0O")
+        {
+            string chars = string.Empty;
+            int count = 0;
+            while (noOfChar > count)
+            {
+                char random = GenerateCharacter(first, last);
+                var excludeSimilarities = without.Contains(random.ToString());
+                if (!excludeSimilarities)
+                {
+                    chars += random;
+                    count++;
+                }
+
+            }
+            return chars;
+        }
+        public int CharCounter(string chars, char first, char last) {
+            
+                int counter = 0;
+                foreach (char c in chars)
+                    if (first <= c && c <= last)
+                        counter++;
+            
+            return counter;
+        }
+        public int SymbolCounter ( string symbols)
+        {
+            int noOfSymbols = 0;
+            int counter = 0;
+            string pass = GennerateSymbolPassword(noOfSymbols, "()/\'~,;.<>{}[]");
+            foreach (char c in symbols)
+                counter++;
+            return counter;
+        }
+   }
 }
 
